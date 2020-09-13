0fc35f847965e0b80ea0094f0f286476f8abb86a
Sardor <mr.narzullaev@gmail.com>
2020-09-13 22:03:27 +0800

    textfield and cards added

--- calls/lib/main.dart
+++ calls/lib/main.dart
@@ -3,10 +3,142 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xFF263238),
+        body: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 15.0),
+                child: new Theme(data: ThemeData(
+                  primaryColor: Colors.white,
+                  primaryColorDark: Colors.red,
+                ), child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: new BorderSide(color: Colors.white)
+                    ),
+                    labelText: 'Search',
+                    prefixIcon: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),)
+              ),
 
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/ava.png'),
+                    ),
+                  ),
+                  title: Text('Ava '),
+                  subtitle: Text('+60123123345'),
+                  trailing: Icon(Icons.call, color: Colors.green,),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/harry.png'),
+                    ),
+                  ),
+                  title: Text('Harry'),
+                  subtitle: Text('+60186773340'),
+                  trailing: Icon(Icons.call, color: Colors.green,),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/jacob.png'),
+                    ),
+                  ),
+                  title: Text('Jacob'),
+                  subtitle: Text('+68236432432'),
+                  trailing: Icon(Icons.call, color: Colors.green),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/mia.png'),
+                    ),
+                  ),
+                  title: Text('Mia'),
+                  subtitle: Text('+69832749823'),
+                  trailing: Icon(Icons.call, color: Colors.green,),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/sophie.png'),
+                    ),
+                  ),
+                  title: Text('Sophie'),
+                  subtitle: Text('+73289479837'),
+                  trailing: Icon(Icons.call, color: Colors.green),
+                ),
+              ),
+
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    radius: 22.0,
+                    backgroundColor: Colors.purpleAccent,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundImage: AssetImage('images/thomas.png'),
+                    ),
+                  ),
+                  title: Text('Thomas'),
+                  subtitle: Text('+87432823462'),
+                  trailing: Icon(Icons.call, color: Colors.green,),
+                ),
+              ),
+
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
