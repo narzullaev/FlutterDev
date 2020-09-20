e1965988e6af79939164675fdf00a12cc0ece29d
Sardor <mr.narzullaev@gmail.com>
2020-09-20 22:00:10 +0800

    built a function

--- books/lib/main.dart
+++ books/lib/main.dart
@@ -4,12 +4,39 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 
+  Expanded buildCategory({MaterialColor color, String image, String title}){
 
+    return Expanded(
+      child: Container(
+        color: color,
+        child: Center(
+          child: ListTile(
+            leading: Text(
+              title,
+              style: TextStyle(
+                fontSize: 22.0,
+                color: Colors.white70
+              ),),
+            trailing: Image.asset('images/$image'),
+          ),
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              buildCategory(color: Colors.red, image:'language.png', title:'Language'),
 
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
