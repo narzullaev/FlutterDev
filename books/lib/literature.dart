d87fd62ada5d58197d9adf0e710b06b90ce40936
Sardor <mr.narzullaev@gmail.com>
2020-09-23 22:59:54 +0800

    literature screen added

---
+++ books/lib/literature.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Languages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+        ),
+        title: Text('Literature'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
+      ),
+    );
+  }
+}
